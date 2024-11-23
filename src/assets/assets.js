43a19ab1a80eb45eb56acf09edbb20c9a71fb8a9
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2024-11-23 12:42:15 +0530

    railway api

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,7 +6,8 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 // export const url = 'http://localhost:4000'
-export const url = 'https://fooddel-backend-pf0z.onrender.com'
+export const url = 'https://fooddelbackend.up.railway.app'
+// export const url = 'https://fooddel-backend-pf0z.onrender.com'
 export const currency = '₹'
 
 export const assets ={
