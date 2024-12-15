605c3d06126926ae3623d866597d4d299e52b4e2
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2024-12-15 21:53:40 +0530

    base url changes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,7 +6,8 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 // export const url = 'http://localhost:4000'
-export const url = 'https://fooddelbackend.up.railway.app'
+// export const url = 'https://fooddelbackend.up.railway.app'
+export const url = 'https://d9543e65-8ee4-4e53-9b4a-808fdfa65bd7.e1-us-east-azure.choreoapps.dev'
 // export const url = 'https://fooddel-backend-pf0z.onrender.com'
 export const currency = '₹'
 
