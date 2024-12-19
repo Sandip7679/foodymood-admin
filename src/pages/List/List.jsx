51508291fe8cb0588969404c20ec08a6ce0f3034
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2024-12-19 17:29:47 +0530

    loader btn

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import { url, currency } from '../../assets/assets'
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import { url, currency } from "../../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchList = async () => {
-    const response = await axios.get(`${url}/api/food/list`)
+    const response = await axios.get(`${url}/api/food/list`);
     if (response.data.success) {
       setList(response.data.data);
+    } else {
+      toast.error("Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   const removeFood = async (foodId) => {
+    setIsLoading(true);
     const response = await axios.post(`${url}/api/food/remove`, {
-      id: foodId
-    })
+      id: foodId,
+    });
     await fetchList();
+    setIsLoading(false);
     if (response.data.success) {
       toast.success(response.data.message);
+    } else {
+      toast.error("Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   useEffect(() => {
     fetchList();
-  }, [])
+  }, []);
 
   return (
-    <div className='list add flex-col'>
+    <div className="list add flex-col">
       <p>All Foods List</p>
-      <div className='list-table'>
+      <div className="list-table">
         <div className="list-table-format title">
           <b>Image</b>
           <b>Name</b>
@@ -48,18 +48,32 @@ const List = () => {
         </div>
         {list.map((item, index) => {
           return (
-            <div key={index} className='list-table-format'>
+            <div key={index} className="list-table-format">
               <img src={`${url}/images/` + item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
-              <p>{currency}{item.price}</p>
-              <p className='cursor' onClick={() => removeFood(item._id)}>x</p>
+              <p>
+                {currency}
+                {item.price}
+              </p>
+              <div className="cross-icon">
+                {/* {!isLoading ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p className="cursor" onClick={() => removeFood(item._id)}>
+                    x
+                  </p>
+                )} */}
+                <p className="cursor" onClick={() => removeFood(item._id)}>
+                  x
+                </p>
+              </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
