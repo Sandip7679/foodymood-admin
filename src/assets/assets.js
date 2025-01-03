13503c309b7fce1dff3b71cd781cf19d87032f40
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-01-03 21:08:46 +0530

    vercel

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -8,9 +8,9 @@ import default_image from './default-image.png'
 
 // export const url = 'http://localhost:4000'
 // export const url = 'https://d9543e65-8ee4-4e53-9b4a-808fdfa65bd7.e1-us-east-azure.choreoapps.dev'
-// export const url = 'https://food-del-backend-kappa.vercel.app'
+export const url = 'https://food-del-backend-kappa.vercel.app'
 // export const url = 'https://fooddel-backend-pf0z.onrender.com'
-export const url = 'https://foodymoodbackend-production.up.railway.app'
+// export const url = 'https://foodymoodbackend-production.up.railway.app'
 export const currency = '₹'
 
 export const assets ={
